e533d6d44b532aeb653adcb2aa8ae66dd958edc1
Felix Parker <flixpar@gmail.com>
2022-11-15 05:24:23 -0500

    move index to templates

--- backend.py
+++ backend.py
@@ -1,8 +1,8 @@
-from flask import Flask, send_file
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-	return send_file("index.html")
+	return render_template("index.html")
